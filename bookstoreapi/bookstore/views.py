aec8345d059c08b58efe2365db0fc968cedb59a9
Ziad <ziadytma@gmail.com>
2023-08-05 17:10:14 +0100

    add function add_to_cart working

--- bookstoreapi/bookstore/views.py
+++ bookstoreapi/bookstore/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404
+
 from rest_framework import generics
 from rest_framework import viewsets
-from .models import Client, Order, Comment, WishList, ShippingInfo, Book, Tag, Category, Post, BookOrder
-from .serializers import *
+from rest_framework.decorators import action
+from bookstore.models import Client, Order, Comment, WishList, ShippingInfo, Book, Tag, Category, Post, BookOrder
+from bookstore.serializers import *
 from django.http import  JsonResponse
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly
 
@@ -74,47 +76,30 @@ class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
 
+    @action(detail=False, methods=["POST"])
+    def add_to_cart(self, request):
+        # user = request.user
+        user = Client.objects.first()
+        book_id = request.data.get('book_id', 0)
+        print("Book id:", book_id)
+        if book_id is None:
+            return Response({'status': "Book ID not provided."}, status=400)
+        else:
+            try:
+                book = Book.objects.get(id=book_id)
+            except Book.DoesNotExist:
+                return Response({'status': "No such book found."}, status=400)
+            
+        print("book:", book)
+        cart_item, created = Cart.objects.get_or_create(user=user)
+        cart_item.books.add(book)
+        return Response({'status': 'Book added to cart successfully'})
 
-
-
-""" def add_to_cart(request, book_id):
-    try:
-        Cart.user = User(Client)
-        books = Book.objects.get(pk=book_id)
-        cart = Cart.objects.create(user= request.user, books=books)
-        cart.save()
-        return Response({"message": "Book added to cart successfully"})
-    except Book.DoesNotExist:
-        return Response({"message": "Book not found"}, status=404)
-    except Cart.DoesNotExist:
-        return Response({"message": "Cart not found"}, status=404) """
-
-
+""" 
 @csrf_exempt
 @api_view(['POST'])
 def add_to_cart(request):
     if request.method == 'POST':
-            book_id = request.POST.get('bookId')
-            if book_id is None:
-             return Response({'status': "Book ID not provided."}, status=400)
-            else:
-            
-             try:
-                book = Book.objects.get(id=book_id)
-             except Book.DoesNotExist:
-                return Response({'status': "No such book found."}, status=400)
-             
-            user = request.user
-             
-            cart_item = Cart.objects.get_or_create(user= user)
-            cart_item.books.add(book)
-            cart_item.save()
-    return Response({'status': 'Book added to cart successfully'})
-
-
-
-    
-"""     if request.method == 'POST':
             book_id = request.POST.get('bookId')
             if book_id is None:
              return Response({'status': "Book ID not provided."}, status=400)
