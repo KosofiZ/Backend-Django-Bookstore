2521f13111431897470f41ce29501d3a2a8cb529
Ziad <ziadytma@gmail.com>
2023-08-07 11:52:40 +0100

    The exact right code for models file this one to get back if I mess  up anything

--- bookstoreapi/bookstore/models.py
+++ bookstoreapi/bookstore/models.py
@@ -1,8 +1,9 @@
 from datetime import timezone
 from django.db import models
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, User
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
+from django.conf import settings
 
 from bookstore import settings as bookstore_settings
 
@@ -17,6 +18,89 @@ class WishList(models.Model):
         return f"Wishlist {self.id}"
     
 
+
+
+
+# class CustomAccountManager(BaseUserManager):
+
+#     def create_superuser(self, email, user_name, first_name, password, **other_fields):
+
+#         other_fields.setdefault('is_staff', True)
+#         other_fields.setdefault('is_superuser', True)
+#         other_fields.setdefault('is_active', True)
+
+#         if other_fields.get('is_staff') is not True:
+#             raise ValueError(
+#                 'Superuser must be assigned to is_staff=True.')
+#         if other_fields.get('is_superuser') is not True:
+#             raise ValueError(
+#                 'Superuser must be assigned to is_superuser=True.')
+
+#         return self.create_user(email, user_name, first_name, password, **other_fields)
+
+#     def create_user(self, email, user_name, first_name, password, **other_fields):
+
+#         if not email:
+#             raise ValueError(_('You must provide an email address'))
+
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, user_name=user_name,
+#                           first_name=first_name, **other_fields)
+#         user.set_password(password)
+#         user.save()
+#         return user
+    
+
+# class Client(AbstractBaseUser, PermissionsMixin):
+
+#     phone_regex = RegexValidator(
+#         regex=r'^\d{10}$',
+#         message="Phone number must be exactly 10 digits."
+#     )
+    
+#     # class Meta(User.Meta):
+#     #     verbose_name = _("Client")
+#     #     verbose_name_plural = _("Clients")
+
+      
+#     email = models.EmailField(_('email address'), unique=True, null = False , blank = False , default='')
+#     user_name = models.CharField(max_length=150, unique=True, null=False, blank=False)
+#     first_name = models.CharField(max_length=150, blank=True, null= True)
+#     phone = models.CharField(_("Phone"), max_length=10, validators=[phone_regex], blank= True )
+#     shipping_info = models.OneToOneField(
+#         'ShippingInfo', verbose_name=_("Shipping Info"), on_delete=models.PROTECT
+#         , blank=True, null=True
+#     )
+#     wishlist = models.OneToOneField(
+#         WishList, verbose_name=_("Wishlist"), related_name='user', on_delete=models.PROTECT,
+#         blank=True
+#     )
+
+#     objects = CustomAccountManager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['user_name', 'first_name']
+
+    
+
+
+#     def get_full_name(self):
+#         return self.first_name
+
+#     def get_short_name(self):
+#         return self.first_name
+ 
+
+#     def __str__(self):
+#          return str(self.username)   
+
+
+#     def save(self, *args, **kwargs):
+#         if not hasattr(self, 'wishlist'):
+#             self.wishlist = WishList.objects.create()
+#         super().save(*args, **kwargs)
+
+
 class Client(User):
 
     phone_regex = RegexValidator(
@@ -29,7 +113,7 @@ class Client(User):
         verbose_name_plural = _("Clients")
 
 
-    phone = models.CharField(_("Phone"), max_length=10, validators=[phone_regex], blank= True  )
+    phone = models.CharField(_("Phone"), max_length=10, validators=[phone_regex], blank= True )
     shipping_info = models.OneToOneField(
         'ShippingInfo', verbose_name=_("Shipping Info"), on_delete=models.PROTECT
         , blank=True, null=True
@@ -39,10 +123,16 @@ class Client(User):
         blank=True
     )
 
-    # def __str__(self):
-    #    return f"{self.first_name} {self.last_name}"
-       
     
+
+
+    def get_full_name(self):
+        return self.first_name
+
+    def get_short_name(self):
+        return self.first_name
+ 
+
     def __str__(self):
          return str(self.username)   
 
@@ -55,7 +145,7 @@ class Client(User):
 
 class Category(models.Model):
 
-    class Meta(User.Meta):
+    class Meta(AbstractBaseUser.Meta):
             verbose_name = _("Category")
             verbose_name_plural = _("Categories")
 
@@ -63,6 +153,7 @@ class Category(models.Model):
 
     def __str__(self):
             return self.name
+    
 
 
 class Tag(models.Model):
@@ -109,7 +200,6 @@ class ShippingInfo(models.Model):
         verbose_name = _("Shipping information")
         verbose_name_plural = _("Shipping information")
 
-    order = models.ForeignKey("Order", on_delete=models.PROTECT, null=True, blank=True)
     name = models.CharField(_("Name"), max_length=50, default="", blank=True)
     address = models.CharField(_("Address"), max_length=255, blank=False)
     city = models.CharField(_("City"), max_length=100, blank=False)
@@ -146,7 +236,11 @@ class Order(models.Model):
 
     user = models.ForeignKey(Client, verbose_name=_("User"), on_delete=models.PROTECT)
     books = models.ManyToManyField(Book, verbose_name=_("Books"), related_name="orders", through="BookOrder")
-    created_at = models.DateTimeField(_("Created_at"),auto_now_add=True)
+    shipping_info = models.ForeignKey(
+        ShippingInfo, verbose_name=_("Shipping Infos"), related_name="orders",
+        on_delete=models.PROTECT
+    )
+    created_at = models.DateTimeField(_("Created_at"), auto_now_add=True)
     status = models.CharField(
         _("Status"), choices=bookstore_settings.ORDER_STATUS_CHOICES,
         default="pending", max_length=32
@@ -195,7 +289,7 @@ class Post(models.Model):
             "-published_at", "title"
         ]
 
-    user = models.ForeignKey(User, verbose_name=_("User"), null= True, on_delete=models.SET_NULL, related_name="posts")
+    user = models.ForeignKey(User , verbose_name=_("User"), null= True, on_delete=models.SET_NULL, related_name="posts")
     title = models.CharField(_("Title "), max_length=256, blank=False)
     content = models.TextField(_("Content"), blank = True)
     categories = models.ManyToManyField(Category, verbose_name=("Categories"), related_name="posts", blank=True)
